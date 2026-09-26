4fbe39e4970681fdc9a5f3395c9efcd528d30060
verhoturov.v9 <verhoturov.v9@rwb.ru>
2026-09-26 18:30:11 +0300

    upd

---
+++ clients/python/wb_api_client/general/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from wb_api_client.general.api.api_api import APIApi
+from wb_api_client.general.api.api import Api
+from wb_api_client.general.api.wbapi_api import WBAPIApi
---
+++ clients/python/wb_api_client/orders_fbw/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from wb_api_client.orders_fbw.api.api import Api
+from wb_api_client.orders_fbw.api.api import Api
+from wb_api_client.orders_fbw.api.api import Api
